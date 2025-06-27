c444a054be588714b8b4a27095f09c16b971ff70
dhruvshibhare <dhruvshivhare16@gmail.com>
2025-06-28 02:32:14 +0530

    Update backend code

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://dfrontend-eta.vercel.app",
+      "https://dfrontend-git-main-dhruvshibhares-projects.vercel.app",
+      "https://dfrontend-4q4881jo8-dhruvshibhares-projects.vercel.app"
+    ],
     methods: ["GET", "POST"]
   }
 });
